16cc94d04c40971df21835eeb3ba840bff60199c
oldhand <68594864@qq.com>
2025-08-26 02:16:17 +0800

    no message

--- install.sh
+++ install.sh
@@ -11,4 +11,10 @@ systemctl disable firewalld
 # 关闭 SELinux 脚本
 setenforce 0
 
+if [ ! -f "images/kube-apiserver-v1.29.3.tar" ]; then
+  cat $PWD/images/images.zip.001 $PWD/images/images.zip.002 $PWD/images/images.zip.003 $PWD/images/images.zip.004 $PWD/images/images.zip.005 > $PWD/images/images.zip
+  unzip $PWD/images/images.zip -d $PWD/images/
+  rm -fr $PWD/images/images.zip
+fi
+
 ansible-playbook -i hosts.ini install_k8s.yml
