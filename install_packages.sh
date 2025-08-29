a407793d36d5ec457e79cdef6b8c14ce16fb3e37
oldhand <68594864@qq.com>
2025-08-29 14:54:44 +0800

    no message

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 rpm -ivh $(pwd)/packages/*.rpm --force --nodeps
+
+ansible-galaxy collection install $(pwd)//kubernetes-core-6.1.0.tar.gz --force
+
+pip install --no-index --find-links=$(pwd)/pip kubernetes PyYAML
